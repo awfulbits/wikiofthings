87c4e119d665ccc8f5cc8fc65581a7dbf01c8126
Julian <19397727+awfulbits@users.noreply.github.com>
2022-04-10 03:39:04 -0600

    Fixed error handling in test page

--- testpage/testpage.go
+++ testpage/testpage.go
@@ -10,29 +10,35 @@ import (
 )
 
 func RunTest(db *database.DB) error {
-	page := &pages.Page{}
 	title := "Hello friend"
-	page.Title = title
-	escapedTitle := page.Escape()
-	page, err := pages.New(escapedTitle, db)
+	page, err := getTestPage(title, db)
+	if err != nil {
+		log.Printf("error fetching test page id: %s - it is likely not present yet", err)
+		if page.Title != title || page.ID <= 0 {
+			err = createTestPage(page, db)
+			if err != nil {
+				return err
+			}
+			log.Print("Test page created - you should only see this message on first run or if there is no database on this system")
+		}
+	}
+	page, err = getTestPage(title, db)
 	if err != nil {
 		return fmt.Errorf("error fetching test page id: %s", err)
 	}
+	log.Print("Test page id should be 1: " + fmt.Sprint(page.ID))
 	err = page.Load(db)
 	if err != nil {
 		return fmt.Errorf("error loading test page: %s", err)
 	}
-	if page.Title != title || page.ID <= 0 {
-		err = createTestPage(page, db)
-		if err != nil {
-			return err
-		}
-		log.Print("Test page created, you should only see this message on first run or if there is no database on this system")
-		return nil
-	} else {
-		log.Print("Test page id should be 1: " + fmt.Sprint(page.ID))
-	}
 	return nil
+}
+
+func getTestPage(title string, db *database.DB) (*pages.Page, error) {
+	page := pages.Page{}
+	page.Title = title
+	escapedTitle := page.Escape()
+	return pages.New(escapedTitle, db)
 }
 
 func createTestPage(page *pages.Page, db *database.DB) error {
